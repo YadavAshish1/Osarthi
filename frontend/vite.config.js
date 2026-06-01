6b3b745d1363303a01db7a1627fbae40e69bd98a
Ashish Yadav <ashishyadav935192@gmail.com>
2026-06-02 00:43:37 +0530

    api url change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
-      '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
+      '/api': { target: 'https://osarthi.onrender.com/', changeOrigin: true },
+      '/uploads': { target: 'https://osarthi.onrender.com/', changeOrigin: true },
     },
   },
 });
